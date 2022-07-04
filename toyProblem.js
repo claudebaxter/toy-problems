2e1d30182bd793e114863671eccd1847eca64929
elborracho420 <e.baxter@dark-coin.io>
2022-07-04 10:25:02 -0500

    Updating toy problem data and skill test question.

---
+++ toyProblem.js
@@ -0,0 +1,25 @@
+//Example of toy problem (solution in bubbleSort.js)
+//We will use an array that contains 3 objects, each with the same fields (title, order and id)
+
+water_categories_data = [
+  {
+    "title": "river",
+    "order": 3,
+    "id": "yqM3w",
+  },
+  {
+    "title": "ocean",
+    "order": 1,
+    "id": "b1en2n",
+  },
+  {
+    "title": "ocean",
+    "order": 2,
+    "id": "blen2n"
+  }
+]
+
+//You'll notice the objects are out of order in the "order" array.
+//Test: write a function that takes an array of numbers as input.
+//It should output an array of those same numbers sorted from lowest to highest.
+//see bubbleSort.js for solution.
